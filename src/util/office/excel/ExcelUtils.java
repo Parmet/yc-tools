b50e3c008da598eaf78f9c88002234dc9338818b
xiaoyuchuan <158215350@qq.com>
2017-04-18 21:49:32 +0800

    工具更新

--- src/util/office/excel/ExcelUtils.java
+++ src/util/office/excel/ExcelUtils.java
@@ -1,4 +1,4 @@
-package util.excel;
+package util.office.excel;
 
 import java.io.BufferedInputStream;
 import java.io.BufferedOutputStream;
--- src/util/office/word/WordOperateUtil.java
+++ src/util/office/word/WordOperateUtil.java
@@ -1,4 +1,4 @@
-package util.word;
+package util.office.word;
 
 import java.io.File;
 import java.io.FileInputStream;
